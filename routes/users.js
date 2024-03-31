3c2381584385727e62ace902d2a230cad00d9ba0
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-03-31 20:12:55 +0530

    changes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose")
 var plm = require("passport-local-mongoose")
 
 // mongoose.connect("mongodb://127.0.0.1:27017/google-drive")
-mongoose.connect("mongodb://0.0.0.0:27017/google-drive")
+// mongoose.connect("mongodb://0.0.0.0:27017/google-drive")
+mongoose.connect(process.env.MONGODB_URL)
 
 var userschema = mongoose.Schema({
   email: String,
