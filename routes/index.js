ddecbf7868824c707316a0c6b4d36742818b870c
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-04-01 00:26:12 +0530

    some change

--- routes/index.js
+++ routes/index.js
@@ -32,8 +32,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 passport.use(new GoogleStrategy({
-  clientID: process.env['GOOGLE_CLIENT_ID'],
-  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: '/oauth2/redirect/google',
   scope: ['email','profile' ]
 }, async function verify(issuer, profile,cb) {
